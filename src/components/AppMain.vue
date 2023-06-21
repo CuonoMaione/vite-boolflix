fc640ba884113d31ee6eaa466d3f479af864aecb
CuonoMaione <cuonomaione99@outlook.it>
2023-06-21 19:17:56 +0200

    add another info about movies get by api

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,17 @@
 <template lang="">
-   <ul>
-    <li v-for="title in titleList">{{ title.original_title}}</li>
-   </ul>
+    <div class="container">
+        <div class="row">
+            <ul class="col-3 mb-5 list-group-numbered" v-for = "title in titleList">
+                <li> {{ title.title}}</li>
+                <li>{{ title.original_title }}</li>
+                <li>{{ title.original_language}}</li>
+                <li>{{ title.vote_average}}</li>
+            </ul>    
+        </div>
+    </div>
+
+    
+   
 </template>
 <script>
 import axios from 'axios';
